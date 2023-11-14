ababd6484b8a640d39ef4e919b575afd53e8df51
wonkyum-kim <dodo12120@naver.com>
2023-11-14 10:08:17 +0900

    feat: 메인 페이지 사진 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
           Keep Running
         </Link>
       </div>
-      <div className='w-full h-full flex'>
+      <div className='w-full h-full flex gap-4'>
         <div className='flex flex-col gap-8 w-full h-2/5 md:w-2/5 md:h-full bg-gray-50 rounded-lg items-center justify-center p-4'>
           <p className='text-gray-800 lg:text-lg xl:text-2xl md:leading-loose'>
             <strong>러닝</strong> 계획을 정하고 성과를 기록해보세요.
@@ -20,7 +21,16 @@ export default function Home() {
             지금 시작해보기
           </button>
         </div>
-        {/* images */}
+        <div className='w-full h-full relative'>
+          <Image
+            alt='running'
+            src='/together.png'
+            fill
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+        </div>
       </div>
     </main>
   );
