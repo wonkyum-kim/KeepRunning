f9bef895560911b7e9b493201aa8fe28a325e495
wonkyum-kim <dodo12120@naver.com>
2023-11-14 10:25:31 +0900

    feat: 반응형 크기로 대회 정보 표시

--- app/ui/dashboard/contests/table.tsx
+++ app/ui/dashboard/contests/table.tsx
@@ -32,39 +32,59 @@ export default function Table({ currentPage, data }: TableProps) {
   const router = useRouter();
 
   return (
-    <div className='min-w-full py-5 px-2 overflow-auto bg-gray-50 rounded-lg hidden md:block'>
-      <table className='min-w-full text-gray-900'>
-        <thead className='text-left text-2xl font-normal'>
-          <tr>
-            <th className='py-8'>날짜</th>
-            <th className='py-8'>대회</th>
-            <th className='py-8'>장소</th>
-            <th className='py-8'>종목</th>
-            <th className='py-8'>주최</th>
-          </tr>
-        </thead>
-        <tbody className='bg-white overflow-x-auto '>
-          {contest.map((contestName, index) => {
-            return (
-              <tr
-                key={contestName}
-                className='whitespace-nowrap h-16 text-left text-lg cursor-pointer hover:bg-gray-100 border-b last-of-type:border-none [&:first-child>th:first-child]:rounded-tl-lg [&:first-child>th:last-child]:rounded-tr-lg [&:last-child>th:first-child]:rounded-bl-lg [&:last-child>th:last-child]:rounded-br-lg'
-                onClick={() => {
-                  router.push(`/dashboard/contests/${id[index]}`);
-                }}
-              >
-                <th>
-                  {date[index]} {day[index]}
-                </th>
-                <th>{contestName}</th>
-                <th>{place[index]}</th>
-                <th className='text-red-500'>{length[index]}</th>
-                <th>{host[index]}</th>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className='min-w-full py-5 px-2 overflow-auto bg-gray-50 rounded-lg hidden md:block'>
+        <table className='min-w-full text-gray-900'>
+          <thead className='text-left text-2xl font-normal'>
+            <tr>
+              <th className='py-8'>날짜</th>
+              <th className='py-8'>대회</th>
+              <th className='py-8'>장소</th>
+              <th className='py-8'>종목</th>
+              <th className='py-8'>주최</th>
+            </tr>
+          </thead>
+          <tbody className='bg-white overflow-x-auto '>
+            {contest.map((contestName, index) => {
+              return (
+                <tr
+                  key={contestName}
+                  className='whitespace-nowrap h-16 text-left text-lg cursor-pointer hover:bg-gray-100 border-b last-of-type:border-none [&:first-child>th:first-child]:rounded-tl-lg [&:first-child>th:last-child]:rounded-tr-lg [&:last-child>th:first-child]:rounded-bl-lg [&:last-child>th:last-child]:rounded-br-lg'
+                  onClick={() => {
+                    router.push(`/dashboard/contests/${id[index]}`);
+                  }}
+                >
+                  <th>
+                    {date[index]} {day[index]}
+                  </th>
+                  <th>{contestName}</th>
+                  <th>{place[index]}</th>
+                  <th className='text-red-500'>{length[index]}</th>
+                  <th>{host[index]}</th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className='flex flex-col gap-4 md:hidden min-w-full h-full rounded-lg'>
+        {contest.map((contestName, index) => {
+          return (
+            <div
+              key={contestName}
+              className='w-full max-h-[12%] bg-gray-50 rounded-lg flex items-center justify-between cursor-pointer hover:bg-gray-100 text-lg p-4'
+              onClick={() => {
+                router.push(`/dashboard/contests/${id[index]}`);
+              }}
+            >
+              <div>{contestName}</div>
+              <div>
+                {date[index]} {day[index]}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
