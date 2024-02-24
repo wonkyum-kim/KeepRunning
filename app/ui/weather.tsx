f5392d682093fd6f4602af7393fb5f670c27f8aa
wonkyum-kim <dodo12120@naver.com>
2024-02-24 12:19:03 +0900

    chore: 지역 이름 표시 안하도록 수정

--- app/ui/weather.tsx
+++ app/ui/weather.tsx
@@ -6,7 +6,6 @@ import { Droplet, Thermometer, Wind } from 'lucide-react';
 
 export default function Weather() {
   const weather = useWeather();
-  console.log(weather);
 
   return (
     <div className='bg-blue-100 rounded-lg min-w-[180px] md:w-[350px] h-full flex items-center justify-center'>
@@ -20,9 +19,6 @@ export default function Weather() {
             className='md:w-[100px] md:h-[100px]'
           />
           <div className='w-full h-full flex flex-col items-center justify-center md:gap-4'>
-            <div className='w-full pl-4 font-bold md:text-lg'>
-              {weather.name}
-            </div>
             <div className='w-full text-xs md:text-lg flex items-center justify-around'>
               <div className='flex items-center'>
                 <Thermometer color='red' fill='red' /> {weather.temp}°C
