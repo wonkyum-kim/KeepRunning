0e846dfb7d69137be1a2450645b5be459f319d1a
wonkyum-kim <dodo12120@naver.com>
2024-02-28 12:09:48 +0900

    feat: new dashboard

--- app/ui/main/dashboard.tsx
+++ app/ui/main/dashboard.tsx
@@ -1,14 +1,41 @@
+import { MoreHorizontal } from 'lucide-react';
+import Link from 'next/link';
+
 export default function Dashboard() {
   return (
-    <div className='bg-blue-400 w-full min-h-[200px] grid grid-cols-4 place-items-center rounded-lg'>
-      <div className='w-full text-center'>기록</div>
-      <div className='w-full text-center'>기록</div>
-      <div className='w-full text-center'>기록</div>
-      <div className='w-full text-center'>기록</div>
-      <div className='w-full text-center'>기록</div>
-      <div className='w-full text-center'>기록</div>
-      <div className='w-full text-center'>기록</div>
-      <div className='w-full text-center'>기록</div>
-    </div>
+    <aside className='bottom-0 left-0 z-10 fixed w-full flex items-center justify-center h-[55px]'>
+      <nav className='bg-[#eee] w-full h-full md:w-[768px] grid grid-cols-4'>
+        <Link
+          href={'/'}
+          className='flex flex-col items-center justify-center select-none'
+        >
+          <div>🏠</div>
+          <div>홈</div>
+        </Link>
+        <Link
+          href={'/record'}
+          className='flex flex-col items-center justify-center select-none'
+        >
+          <div>🖼️</div>
+          <div>기록 인증</div>
+        </Link>
+        <Link
+          href={'/stat'}
+          className='flex flex-col items-center justify-center select-none'
+        >
+          <div>📌</div>
+          <div>통계</div>
+        </Link>
+        <Link
+          href={'/more'}
+          className='flex flex-col items-center justify-center select-none'
+        >
+          <div>
+            <MoreHorizontal />
+          </div>
+          <div>더보기</div>
+        </Link>
+      </nav>
+    </aside>
   );
 }
