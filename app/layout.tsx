eec56bbd0b78c729274f36567a157484c31b6429
wonkyum-kim <dodo12120@naver.com>
2024-02-29 10:37:04 +0900

    fix: main title height
    - 높이가 일정하게 유지되도록 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased flex flex-col gap-4 w-full h-full p-4 md:w-[768px] m-auto`}
       >
-        <div className='h-20 w-full rounded-lg bg-blue-500 p-2 md:p-4 md:h-40 flex justify-between items-center'>
+        <div className='w-full rounded-lg bg-blue-500 p-2 md:p-4 md:h-40 flex justify-between items-center'>
           <Link
             href='/'
             className='h-full text-white font-bold text-xl flex items-center select-none cursor-pointer justify-center md:justify-start md:items-end md:text-6xl md:p-4'
