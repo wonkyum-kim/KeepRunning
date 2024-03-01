7e53441015ee0879c3850cfbb420a0b5930df300
wonkyum-kim <dodo12120@naver.com>
2024-03-01 22:16:55 +0900

    feat: more 페이지 업데이트

--- app/more/page.tsx
+++ app/more/page.tsx
@@ -2,11 +2,21 @@ import Link from 'next/link';
 
 export default function MorePage() {
   return (
-    <div className='w-full flex flex-col p-4'>
-      <Link href='/stat'>📌 러닝 통계 비교</Link>
-      <Link href='contest'>📅 대회 일정 확인</Link>
-      <div>TODO: cal heatmap 라이브러리 사용한 시각화</div>
-      <div>TODO: 개발자 정보</div>
+    <div className='w-full flex flex-col gap-4 text-xl font-bold'>
+      <h1 className='px-4'>더보기</h1>
+      <hr />
+      <Link href='/stat' className='hover:bg-[#eee] p-4 rounded-lg'>
+        📌 러닝 통계 비교
+      </Link>
+      <Link href='/contest' className='hover:bg-[#eee] p-4 rounded-lg'>
+        📅 대회 일정 확인
+      </Link>
+      <div className='hover:bg-[#eee] p-4 rounded-lg cursor-not-allowed'>
+        TODO: 내 일정
+      </div>
+      <div className='hover:bg-[#eee] p-4 rounded-lg cursor-not-allowed'>
+        TODO: 개발자 정보
+      </div>
     </div>
   );
 }
